b1db5e7a95382a4216fac90ed0b4b03578de5d1d
NilsSonntag <93387943+NilsSonntag@users.noreply.github.com>
2025-08-06 18:13:46 +0000

    move slice and block.h to ouichefs.h

--- ouichefs.h
+++ ouichefs.h
@@ -129,6 +129,23 @@ extern struct kobject *ouichefs_sysfs_dir;
 int ouichefs_sysfs_init(struct super_block *sb);
 void ouichefs_sysfs_exit(struct super_block *sb);
 
+/* block functions */
+int large_get_start(struct inode *inode, loff_t pos, struct buffer_head **bh,
+		    char **start, bool create);
+void shrink_multiblock_file(struct file *file);
+
+/* slice functions */
+int read_sliced_get_start(struct inode *inode, loff_t pos,
+			  struct buffer_head **bh, char **start);
+int write_sliced_get_start(struct inode *inode, loff_t pos, size_t count,
+			   struct buffer_head **bh, char **start,
+			   sector_t *new_index_block);
+int free_sliced_file(struct inode *inode);
+int put_slices(struct super_block *sb, sector_t block, uint32_t starting_slice,
+	       uint32_t nr_slices);
+int remove_from_partial_list(struct super_block *sb, sector_t block,
+			     struct ouichefs_sliced_block *s_block);
+
 /* file functions */
 extern const struct file_operations ouichefs_file_ops;
 extern const struct file_operations ouichefs_dir_ops;
